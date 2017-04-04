2355aeba2dee8d5179c1c79e3051e6cc88d8b450
chenfanhan <cfh@CFHdeMacBook-Pro.local>
2017-04-04 10:48:14 +0800

    新建的
    你说呢

---
+++ xinjian/xinjian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xinjian
+//
+//  Created by CFH on 2017/4/4.
+//  Copyright © 2017年 CFH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xinjian/xinjian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xinjian
+//
+//  Created by CFH on 2017/4/4.
+//  Copyright © 2017年 CFH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
